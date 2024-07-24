deff4539ce390a7eb628e8d4f66f8f713d7d70af
Aidan Froggatt <aidanfr@live.ca>
2024-07-23 21:46:46 -0400

    updating eslint, updating tests for nav button

--- components/NavButton.test.tsx
+++ components/NavButton.test.tsx
@@ -1,12 +1,29 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import NavButton from "./NavButton";
 
 describe("NavButton", () => {
   it("renders correctly", () => {
-    const { getByTestId } = render(<NavButton route="/test" text="Test" />);
+    const { getByTestId } = render(
+      <NavButton text="Test" onPress={() => {}} />
+    );
 
     // Verify if the button is rendered
     const button = getByTestId("nav-button");
     expect(button).toBeTruthy();
+  });
+
+  it("calls onPress when pressed", () => {
+    const mockOnPress = jest.fn();
+    const { getByTestId } = render(
+      <NavButton text="Test" onPress={mockOnPress} />
+    );
+
+    const button = getByTestId("nav-button");
+
+    // Simulate a press event
+    fireEvent.press(button);
+
+    // Verify that the onPress function was called
+    expect(mockOnPress).toHaveBeenCalled();
   });
 });
