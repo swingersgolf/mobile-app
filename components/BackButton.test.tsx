8dc197f40cedd6d762479f41108ed0b054b055f5
Aidan Froggatt <aidanfr@live.ca>
2024-07-23 20:51:11 -0400

    updated tests for backbutton

--- components/BackButton.test.tsx
+++ components/BackButton.test.tsx
@@ -1,10 +1,6 @@
-// __tests__/BackButton.test.tsx
-
-import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import { router } from "expo-router";
 import BackButton from "./BackButton";
-import { ThemeProvider } from "@/contexts/ThemeContext";
 
 // Mock router.back function
 jest.mock("expo-router", () => ({
@@ -13,22 +9,9 @@ jest.mock("expo-router", () => ({
   },
 }));
 
-// Mock ThemeContext
-const mockTheme = {
-  backgroundPrimary: "black", // Replace with the expected theme color
-};
-
-const MockThemeProvider: React.FC = ({ children }) => (
-  <ThemeProvider value={mockTheme}>{children}</ThemeProvider>
-);
-
 describe("BackButton", () => {
   it("renders correctly", () => {
-    const { getByTestId } = render(
-      <MockThemeProvider>
-        <BackButton />
-      </MockThemeProvider>,
-    );
+    const { getByTestId } = render(<BackButton />);
 
     // Verify if the button is rendered
     const button = getByTestId("back-button");
@@ -36,11 +19,7 @@ describe("BackButton", () => {
   });
 
   it("triggers router.back when pressed", () => {
-    const { getByTestId } = render(
-      <MockThemeProvider>
-        <BackButton />
-      </MockThemeProvider>,
-    );
+    const { getByTestId } = render(<BackButton />);
 
     // Simulate button press
     const button = getByTestId("back-button");
