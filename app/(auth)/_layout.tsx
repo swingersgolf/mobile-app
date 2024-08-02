ead4f8dcbb53b32fd6a2a6fe6a570e668c086fef
Aidan Froggatt <aidanfr@live.ca>
2024-08-01 20:52:44 -0400

    updated auth with stack navigation for improved ux

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,55 +1,23 @@
-import { Slot, useRouter } from "expo-router";
-import {
-  Keyboard,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
-import BackArrow from "@/components/BackArrow";
-import Icon from "@/assets/branding/Icon.svg";
-import { colors } from "@/constants/Colors";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import AuthHeader from "@/components/AuthHeader";
+import { Stack } from "expo-router";
 
 const UnauthorizedLayout = () => {
-  const router = useRouter();
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView id="auth-screen" style={styles.authScreen}>
-        <View id="navbar" style={styles.navbar}>
-          <BackArrow
-            style={styles.backArrow}
-            onPress={() => router.replace("/")}
-          />
-          <Icon style={styles.icon} height={50} width={50} />
-        </View>
-        <Slot />
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        contentStyle: { padding: 20 },
+        header: () => AuthHeader(),
+      }}
+    >
+      <Stack.Screen
+        name="landing"
+        options={{ headerShown: false, contentStyle: { padding: 0 } }}
+      />
+      <Stack.Screen name="login" options={{}} />
+      <Stack.Screen name="register" options={{}} />
+    </Stack>
   );
 };
 
 export default UnauthorizedLayout;
-
-const styles = StyleSheet.create({
-  authScreen: {
-    flex: 1,
-    padding: 20,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: colors.white,
-    gap: 40,
-  },
-  navbar: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  backArrow: {
-    position: "absolute",
-    left: 0,
-  },
-  icon: {},
-});
---
+++ components/AuthHeader.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const AuthHeader = () => {
+  return <View></View>;
+};
+
+export default AuthHeader;
