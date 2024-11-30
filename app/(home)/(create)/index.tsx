8ac7ed57f2ff23e96504978f18e2c8ba03776b7b
Aidan Froggatt <aidanfr@live.ca>
2024-11-30 15:41:36 -0500

    updating create round screen to use flat list enabling vertical scroll

--- app/(home)/(create)/index.tsx
+++ app/(home)/(create)/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { Text, View, Keyboard, TouchableOpacity } from "react-native";
+import { Text, View, Keyboard, TouchableOpacity, FlatList } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import { useFocusEffect } from "@react-navigation/native";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -199,250 +199,272 @@ const CreateScreen = () => {
     hideDatePicker();
   };
 
-  return (
-    <View style={createStyles.container}>
-      {loading ? (
-        <View style={createStyles.spinnerContainer}>
-          <Spinner />
-        </View>
-      ) : (
-        <>
-          <View id="new-post-form" style={formStyles.form}>
-            <Text style={GlobalStyles.h3}>Details</Text>
-            <View style={formStyles.inputWrapper}>
-              <Controller
-                control={control}
-                name="golfCourse"
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <>
-                    {value && (
-                      <Text
-                        style={[
-                          formStyles.formInputTitle,
-                          errors.golfCourse && formStyles.formInputTitleError,
-                        ]}
-                      >
-                        Golf Course
-                      </Text>
-                    )}
-                    <Dropdown
-                      search
-                      style={[
-                        formStyles.formInput,
-                        errors.golfCourse && formStyles.invalidInput,
-                      ]}
-                      data={golfCourses.map((course) => ({
-                        label: course.name, // Set label to the course name
-                        value: course.id, // Set value to the course id
-                      }))}
-                      labelField="label"
-                      valueField="value"
-                      placeholder="Golf Course"
-                      placeholderStyle={formStyles.placeholderStyle}
-                      selectedTextStyle={{
-                        color: colors.neutral.dark,
-                        fontSize: 14,
-                      }}
-                      itemTextStyle={{
-                        color: colors.neutral.dark,
-                        fontSize: 14,
-                      }}
-                      value={value}
-                      onChange={(item) => {
-                        onChange(item.value); // Now this will be the course id
-                      }}
-                      onFocus={() => Keyboard.dismiss()}
-                      renderRightIcon={() =>
-                        errors.golfCourse ? null : (
-                          <MaterialIcons
-                            name="arrow-drop-down"
-                            size={14}
-                            color={colors.neutral.medium}
-                          />
-                        )
-                      }
-                    />
-                    {errors.golfCourse && (
-                      <InFormAlert error={errors.golfCourse.message} />
-                    )}
-                  </>
-                )}
-              />
-            </View>
-
-            {/* Slots Dropdown */}
-            <View style={formStyles.inputWrapper}>
-              <Controller
-                control={control}
-                name="slots"
-                render={({ field: { onChange, value } }) => (
-                  <>
-                    {value && (
-                      <Text
-                        style={[
-                          formStyles.formInputTitle,
-                          errors.slots && formStyles.formInputTitleError,
-                        ]}
-                      >
-                        Group Size
-                      </Text>
-                    )}
-                    <Dropdown
-                      style={[
-                        formStyles.formInput,
-                        errors.slots && formStyles.invalidInput,
-                      ]}
-                      data={group_size.map((course) => ({
-                        label: course,
-                        value: course,
-                      }))}
-                      labelField="label"
-                      valueField="value"
-                      placeholder="Group size"
-                      placeholderStyle={formStyles.placeholderStyle}
-                      selectedTextStyle={{
-                        color: colors.neutral.dark,
-                        fontSize: 14,
-                      }}
-                      itemTextStyle={{
-                        color: colors.neutral.dark,
-                        fontSize: 14,
-                      }}
-                      value={value}
-                      onChange={(item) => onChange(item.value)}
-                      renderRightIcon={() =>
-                        errors.slots ? null : (
-                          <MaterialIcons
-                            name="arrow-drop-down"
-                            size={14}
-                            color={colors.neutral.medium}
-                          />
-                        )
-                      }
-                    />
-                    {errors.slots && (
-                      <InFormAlert error={errors.slots.message} />
-                    )}
-                  </>
-                )}
-              />
-            </View>
-
-            {/* DateTime Picker */}
-            <View style={formStyles.inputWrapper}>
-              <Controller
-                control={control}
-                name="datetime"
-                render={({ field: { value } }) => (
-                  <>
-                    {value && (
-                      <Text
-                        style={[
-                          formStyles.formInputTitle,
-                          errors.datetime && formStyles.formInputTitleError,
-                        ]}
-                      >
-                        Date and time
-                      </Text>
-                    )}
-                    <TouchableOpacity
-                      onPress={showDatePicker}
-                      style={[
-                        formStyles.formInput,
-                        errors.datetime && formStyles.invalidInput,
-                      ]}
-                    >
-                      <Text style={formStyles.placeholderStyle}>
-                        {selectedDate
-                          ? formatDateDayMonthTime(selectedDate)
-                          : "Date and time"}
-                      </Text>
-                    </TouchableOpacity>
-                    <DateTimePickerModal
-                      isVisible={isDatePickerVisible}
-                      mode="date"
-                      date={selectedDate || new Date()}
-                      onConfirm={handleConfirm}
-                      onCancel={hideDatePicker}
-                    />
-                    {errors.datetime && (
-                      <InFormAlert error={errors.datetime.message} />
-                    )}
-                  </>
-                )}
-              />
-            </View>
-
-            <Text style={GlobalStyles.h3}>Preferences</Text>
-            <View style={(formStyles.inputWrapper, formStyles.preferencesForm)}>
-              {preferencesList.map((preference) => (
-                <View
-                  key={preference.id}
+  const renderGolfCourseDropdown = () => {
+    return (
+      <View style={formStyles.inputWrapper}>
+        <Controller
+          control={control}
+          name="golfCourse"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <>
+              {value && (
+                <Text
                   style={[
-                    formStyles.preferenceRow,
-                    errors.preferences?.[preference.id]
-                      ? formStyles.preferenceRowError
-                      : null,
+                    formStyles.formInputTitle,
+                    errors.golfCourse && formStyles.formInputTitleError,
                   ]}
                 >
-                  <View
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      flexDirection: "row",
-                      gap: 5,
-                    }}
-                  >
-                    <PreferenceIcon preference={preference.label} />
-                    <Text style={formStyles.preferenceLabel}>
-                      {preference.label}
-                    </Text>
-                  </View>
-                  <View style={formStyles.preferenceOptions}>
-                    {["preferred", "disliked", "indifferent"].map((status) => (
-                      <Controller
-                        key={status}
-                        control={control}
-                        name={`preferences.${preference.id}`}
-                        render={({ field: { onChange, value } }) => (
-                          <TouchableOpacity
-                            style={[
-                              formStyles.preferenceButton,
-                              value === status && formStyles.selectedButton,
-                            ]}
-                            onPress={() => onChange(status)}
-                          >
-                            <Text
-                              style={[
-                                formStyles.preferenceButtonText,
-                                value === status &&
-                                  formStyles.selectedButtonText,
-                              ]}
-                            >
-                              {preferenceLabelMap[status]}
-                            </Text>
-                          </TouchableOpacity>
-                        )}
-                      />
-                    ))}
-                  </View>
-                </View>
-              ))}
-            </View>
-
-            {error && <Alert error={error} />}
-            <View style={RoundStyles.textButtonContainer}>
-              <TextButton
-                text="Create Post"
-                onPress={handleSubmit(handleCreateRound)}
-                textColor={colors.neutral.light}
-                backgroundColor={colors.primary.default}
+                  Golf Course
+                </Text>
+              )}
+              <Dropdown
+                search
+                style={[
+                  formStyles.formInput,
+                  errors.golfCourse && formStyles.invalidInput,
+                ]}
+                data={golfCourses.map((course) => ({
+                  label: course.name, // Set label to the course name
+                  value: course.id, // Set value to the course id
+                }))}
+                labelField="label"
+                valueField="value"
+                placeholder="Golf Course"
+                placeholderStyle={formStyles.placeholderStyle}
+                selectedTextStyle={{
+                  color: colors.neutral.dark,
+                  fontSize: 14,
+                }}
+                itemTextStyle={{
+                  color: colors.neutral.dark,
+                  fontSize: 14,
+                }}
+                value={value}
+                onChange={(item) => {
+                  onChange(item.value); // Now this will be the course id
+                }}
+                onFocus={() => Keyboard.dismiss()}
+                renderRightIcon={() =>
+                  errors.golfCourse ? null : (
+                    <MaterialIcons
+                      name="arrow-drop-down"
+                      size={14}
+                      color={colors.neutral.medium}
+                    />
+                  )
+                }
               />
-            </View>
+              {errors.golfCourse && (
+                <InFormAlert error={errors.golfCourse.message} />
+              )}
+            </>
+          )}
+        />
+      </View>
+    );
+  };
+
+  const renderSlotsDropdown = () => {
+    return (
+      <View style={formStyles.inputWrapper}>
+        <Controller
+          control={control}
+          name="slots"
+          render={({ field: { onChange, value } }) => (
+            <>
+              {value && (
+                <Text
+                  style={[
+                    formStyles.formInputTitle,
+                    errors.slots && formStyles.formInputTitleError,
+                  ]}
+                >
+                  Group Size
+                </Text>
+              )}
+              <Dropdown
+                style={[
+                  formStyles.formInput,
+                  errors.slots && formStyles.invalidInput,
+                ]}
+                data={group_size.map((course) => ({
+                  label: course,
+                  value: course,
+                }))}
+                labelField="label"
+                valueField="value"
+                placeholder="Group size"
+                placeholderStyle={formStyles.placeholderStyle}
+                selectedTextStyle={{
+                  color: colors.neutral.dark,
+                  fontSize: 14,
+                }}
+                itemTextStyle={{
+                  color: colors.neutral.dark,
+                  fontSize: 14,
+                }}
+                value={value}
+                onChange={(item) => onChange(item.value)}
+                renderRightIcon={() =>
+                  errors.slots ? null : (
+                    <MaterialIcons
+                      name="arrow-drop-down"
+                      size={14}
+                      color={colors.neutral.medium}
+                    />
+                  )
+                }
+              />
+              {errors.slots && <InFormAlert error={errors.slots.message} />}
+            </>
+          )}
+        />
+      </View>
+    );
+  };
+
+  const renderDatePicker = () => {
+    return (
+      <View style={formStyles.inputWrapper}>
+        <Controller
+          control={control}
+          name="datetime"
+          render={({ field: { value } }) => (
+            <>
+              {value && (
+                <Text
+                  style={[
+                    formStyles.formInputTitle,
+                    errors.datetime && formStyles.formInputTitleError,
+                  ]}
+                >
+                  Date and time
+                </Text>
+              )}
+              <TouchableOpacity
+                onPress={showDatePicker}
+                style={[
+                  formStyles.formInput,
+                  errors.datetime && formStyles.invalidInput,
+                ]}
+              >
+                <Text style={formStyles.placeholderStyle}>
+                  {selectedDate
+                    ? formatDateDayMonthTime(selectedDate)
+                    : "Date and time"}
+                </Text>
+              </TouchableOpacity>
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                date={selectedDate || new Date()}
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+              />
+              {errors.datetime && (
+                <InFormAlert error={errors.datetime.message} />
+              )}
+            </>
+          )}
+        />
+      </View>
+    );
+  };
+
+  const renderPreference = (preference: { id: string; label: string }) => {
+    return (
+      <View style={(formStyles.inputWrapper, formStyles.preferencesForm)}>
+        <View
+          key={preference.id}
+          style={[
+            formStyles.preferenceRow,
+            errors.preferences?.[preference.id]
+              ? formStyles.preferenceRowError
+              : null,
+          ]}
+        >
+          <View
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "row",
+              gap: 5,
+            }}
+          >
+            <PreferenceIcon preference={preference.label} />
+            <Text style={formStyles.preferenceLabel}>{preference.label}</Text>
           </View>
-        </>
-      )}
+          <View style={formStyles.preferenceOptions}>
+            {["preferred", "disliked", "indifferent"].map((status) => (
+              <Controller
+                key={status}
+                control={control}
+                name={`preferences.${preference.id}`}
+                render={({ field: { onChange, value } }) => (
+                  <TouchableOpacity
+                    style={[
+                      formStyles.preferenceButton,
+                      value === status && formStyles.selectedButton,
+                    ]}
+                    onPress={() => onChange(status)}
+                  >
+                    <Text
+                      style={[
+                        formStyles.preferenceButtonText,
+                        value === status && formStyles.selectedButtonText,
+                      ]}
+                    >
+                      {preferenceLabelMap[status]}
+                    </Text>
+                  </TouchableOpacity>
+                )}
+              />
+            ))}
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const formData = [
+    { key: "golfCourse", component: renderGolfCourseDropdown },
+    { key: "slots", component: renderSlotsDropdown },
+    { key: "datetime", component: renderDatePicker },
+    ...preferencesList.map((pref) => ({
+      key: `preferences.${pref.id}`,
+      component: () => renderPreference(pref),
+    })),
+  ];
+
+  const renderItem = ({ item }: { item: (typeof formData)[0] }) => (
+    <View style={formStyles.inputWrapper}>{item.component()}</View>
+  );
+
+  return loading ? (
+    <View style={createStyles.spinnerContainer}>
+      <Spinner />
     </View>
+  ) : (
+    <FlatList
+      data={formData}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.key}
+      contentContainerStyle={createStyles.container}
+      ListHeaderComponent={() => <Text style={GlobalStyles.h3}>Details</Text>}
+      ListFooterComponent={() => (
+        <View>
+          {error && <Alert error={error} />}
+          <View style={RoundStyles.textButtonContainer}>
+            <TextButton
+              text="Create Post"
+              onPress={handleSubmit(handleCreateRound)}
+              textColor={colors.neutral.light}
+              backgroundColor={colors.primary.default}
+            />
+          </View>
+        </View>
+      )}
+    />
   );
 };
 
